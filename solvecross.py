d93808c519aae9eb388c37b7089889711fa33d7c
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-08-14 04:55:16 +0300

    Continued writng the cross solving

--- solvecross.py
+++ solvecross.py
@@ -12,8 +12,32 @@ def blue_right_center(cube):
         rotated_cube = turn(cube, z, deg)
         if rotated_cube[(0, 10, 0)] == 'b': return (rotated_cube, (z, deg))
 
-def white_blue_edge(cube):
-    pass
+def color_fits(cube, face, color_pair):
+    return (cube[face] == color_1 or cube[face] == color_2)
+
+top_edges = [{(0, 9, 10), (0, 10, 9)}, {(9, 0, 10), (10, 0, 9)},
+    {(0, -9, 10), (0, -10, 9)}, {(-9, 0, 10), (-10, 0, 9)}]
+bottom_edges = [{(0, 9, -10), (0, 10, -9)}, {(9, 0, -10), (10, 0, -9)},
+    {(0, -9, -10), (0, -10, -9)}, {(-9, 0, -10), (-10, 0, -9)}]
+color_pairs = [('w', 'b'), ('w', 'r'), ('w', 'g'), ('w', 'o')]
+
+def place_top_edge(cube, order):
+    new_cube, rotations = cube, []
+    current, color_pair = top_edges[order], color_pairs[order]
+    for edge in top_edges:
+        if {color_fits(cube, face, color_pair) for face in edge} == {True}:
+            if edge == current: break
+            pass # Direction depend on the edge.
+            return (new_cube, rotations)
+    for edge in bottom_edges:
+        if {color_fits(cube, face, color_pair) for face in edge} == {True}:
+            pass # Direction depend on the edge.
+            return (new_cube, rotations)
+
+def fit_top_edge(cube, order):
+    new_cube, rotations = place_top_edge(cube)
+    pass # If wrong color at the top: do required rotations.
+    return (new_cube, rotations)
 
 def make_a_cross(cube):
     instructions = {}
@@ -24,7 +48,7 @@ def make_a_cross(cube):
     step_2 = blue_right_center(cube_1)
     cube_2 = step_2[0]
     rotations.append(step_2[1])
-    step_3 = white_blue_edge(cube_3)
+    step_3 = fit_top_edge(cube_3, 0)
     cube_3 = step_3[0]
     rotations.append(step_3[1])
     pass
