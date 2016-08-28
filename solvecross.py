7075281d9402bc173e7d19c4737809100d112687
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-08-28 03:30:07 +0300

    Continued debugging solvecross

--- solvecross.py
+++ solvecross.py
@@ -15,35 +15,58 @@ color_pairs = [('w', 'b'), ('w', 'o'), ('w', 'g'), ('w', 'r')]
 side_rot = [['R'], ['B'], ['L'], ['F']]
 side_inv = [['Ri'], ['Bi'], ['Li'], ['Fi']]
 
-def place_top_edge(cube, order):
+def place_top_edge(cube, edge, current, color_pair):
     new_cube, rotations = cube, []
+    if edge == current:
+        return (new_cube, rotations)
+    else:
+        new_cube = rotate(new_cube, side_rot[top_edges.index(edge)] * 2)
+        rotations += (side_rot[top_edges.index(edge)] * 2)
+    for edge in bottom_edges: # This should be a separate function.
+        if {color_fits(new_cube, face, color_pair) for face in edge} == {True}:
+            next_step = place_bottom_edge(new_cube, edge)
+            new_cube = next_step[0]
+            rotations += next_step[1]
+    return (new_cube, rotations)
+
+def place_side_edge(cube, edge):
+    new_cube, rotations = cube, []
+    new_cube = rotate(new_cube, side_rot[side_edges.index(edge)])
+    rotations += (side_rot[side_edges.index(edge)])
+    for edge in bottom_edges: # This should be a separate function.
+        if {color_fits(new_cube, face, color_pair) for face in edge} == {True}:
+            next_step = place_bottom_edge(new_cube, edge)
+            new_cube = next_step[0]
+            rotations += next_step[1]
+    return (new_cube, rotations)
+
+def place_bottom_edge(cube, edge):
+    new_cube, rotations = cube, []
+    n = bottom_edges.index(edge) - order
+    if n > 0:
+        new_cube = rotate(new_cube, ['D'] * n)
+        rotations += (['D'] * n)
+    if n < 0:
+        new_cube = rotate(new_cube, ['Di'] * -n)
+        rotations += (['Di']  * -n)
+    new_cube = rotate(new_cube, side_rot[bottom_edges.index(edge)] * 2)
+    rotations += (side_rot[bottom_edges.index(edge)] * 2)
+    return (new_cube, rotations)
+
+def place_edge(cube, order):
     current, color_pair = top_edges[order], color_pairs[order]
     for edge in top_edges:
         if {color_fits(cube, face, color_pair) for face in edge} == {True}:
-            if edge == current:
-                return (new_cube, rotations)
-            else:
-                new_cube = rotate(new_cube, side_rot[top_edges.index(edge)] * 2)
-                rotations += (side_rot[top_edges.index(edge)] * 2)
+            return place_top_edge(cube, edge, current)
     for edge in side_edges:
         if {color_fits(cube, face, color_pair) for face in edge} == {True}:
-            new_cube = rotate(new_cube, side_rot[side_edges.index(edge)])
-            rotations += (side_rot[side_edges.index(edge)])
+            return place_side_edge(cube, edge)
     for edge in bottom_edges:
         if {color_fits(cube, face, color_pair) for face in edge} == {True}:
-            n = bottom_edges.index(edge) - order
-            if n > 0:
-                new_cube = rotate(new_cube, ['D'] * n)
-                rotations += (['D'] * n)
-            if n < 0:
-                new_cube = rotate(new_cube, ['Di'] * -n)
-                rotations += (['Di']  * -n)
-        new_cube = rotate(new_cube, side_rot[bottom_edges.index(edge)] * 2)
-        rotations += (side_rot[bottom_edges.index(edge)] * 2)
-    return (new_cube, rotations)
+            return place_bottom_edge(cube, edge)
 
-def fit_top_edge(cube, order):
-    new_cube, rotations = place_top_edge(cube, order)
+def fit_edge(cube, order):
+    new_cube, rotations = place_edge(cube, order)
     if new_cube[top_edges[order][0]] != 'w':
         rot_sequence = side_inv[order] + ['U'] + side_inv[order - 1] + ['Ui']
         new_cube = rotate(new_cube, rot_sequence)
@@ -53,7 +76,7 @@ def fit_top_edge(cube, order):
 def make_a_cross(cube):
     new_cube, rotations = cube, []
     for order in range(4):
-        new_step = fit_top_edge(new_cube, order)
+        new_step = fit_edge(new_cube, order)
         new_cube = new_step[0]
         rotations += new_step[1]
     return (new_cube, rotations)
