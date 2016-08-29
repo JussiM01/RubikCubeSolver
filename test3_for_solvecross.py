5d84acd42a41b6dae19d20bccd068e825cef5ee4
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-08-30 02:11:05 +0300

    Added a test which checks that rotations match

---
+++ test3_for_solvecross.py
@@ -0,0 +1,17 @@
+from scramblecube import *
+from solvecross import *
+
+"""Test which checks that make_a_cross(cube)[1] is a rotation sequence for
+which
+
+        rotate(cube, make_a_cross(cube)[1]) == make_a_cross(cube)[0]
+
+as it is supposed to be."""
+
+def test_rotations(num_iter):
+    count = 0
+    for n in range(num_iter):
+        cube = scramble(start_cube)
+        output = make_a_cross(cube)
+        if output[0] == rotate(cube, output[1]): count += 1
+    print('Rotations match ' + str(count) + ' out of ' + str(num_iter) + '.')
