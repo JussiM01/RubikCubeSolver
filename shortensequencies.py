c1fbccf773529f8537b4f92011d89ff71573db54
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-11-29 19:05:06 +0200

    Added a file for shortening the rotation sequencies

---
+++ shortensequencies.py
@@ -0,0 +1,18 @@
+def opposite_rotations(rot1, rot2):
+    return len(rot1) !=len(rot2) and rot1[0] == rot2[0]
+
+def shorten1(seq):
+    for n in range(len(seq) - 1):
+        if opposite_rotations(seq[n], seq[n + 1]):
+            return shorten1(seq[:n] + seq[n + 2:])
+    return seq
+
+def shorten2(seq):
+    for n in range(len(seq) - 2):
+        if seq[n] == seq[n + 1] == seq[n + 2] and len(seq[n]) == 1:
+            return shorten2(seq[:n] + [seq[n] + 'i'] + seq[n + 3:])
+        if seq[n] == seq[n + 1] == seq[n + 2] and len(seq[n]) == 2:
+            return shorten2(seq[:n] + [seq[n][0]] + seq[n + 3:])
+    return seq
+
+def shorten(seq): return shorten1(shorten2(seq))
